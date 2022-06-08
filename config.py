72c74be239f1e701f7d9f0c388451e1107317ec1
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2022-06-08 12:15:56 +0300

    sql

---
+++ config.py
@@ -0,0 +1,5 @@
+host        = 'localhost'
+user        = 'postgres'
+password    = 'postgres'
+db_name     = 'fias'
+port        = 5432
--- dadata.py
+++ dadata.py
@@ -1,5 +1,6 @@
 from dadata import Dadata
 token = "442b94d3eb75c60f41c4212ff90538763c7b3e17"
 dadata = Dadata(token)
-result = dadata.suggest("address", "Лазань")
+result = dadata.suggest("address", "воронеж, пр-т патриотов д.31/2 кв.40")
+
 print(result)
