9436ca2951ec5be79c820ef1f0fc357cc2aa585b
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2022-06-14 12:16:00 +0300

    step

--- dadata.py
+++ dadata.py
@@ -3,4 +3,7 @@ token = "442b94d3eb75c60f41c4212ff90538763c7b3e17"
 dadata = Dadata(token)
 result = dadata.suggest("address", "воронеж, пр-т патриотов д.31/2 кв.40")
 
-print(result)
+info = str(result)
+info = str(info[info.find('value'):info.find("',")])
+
+print(info)
